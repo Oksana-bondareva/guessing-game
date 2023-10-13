4f605ccd1717dad1783e98964e98dfe047f772d4
Oksana-bondareva <oksanchik252@gmail.com>
2023-10-13 23:43:12 +0300

    feat: add solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,24 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.num = this.max - this.min;
+        this.num = Math.floor(this.num / 2)
+        this.num = this.max - this.num;
+        this.middle = this.num;
+        return this.middle;
     }
 
     lower() {
-
+        this.max = this.middle;
     }
 
     greater() {
-
+        this.min = this.middle;
     }
 }
 
